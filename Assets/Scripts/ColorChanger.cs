c9488e82bf249a1c1ae67ed1a1cb5863ad76f484
Aimar Gonzalez <aimar.mel@gmail.com>
2025-03-11 14:08:49 +0100

    add ColorChanger component

---
+++ Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+namespace ForestRoyale
+{
+    public class ColorChanger : MonoBehaviour
+    {
+        private Renderer meshRenderer;
+        private Color[] colors = { Color.red, Color.green, Color.blue, Color.yellow, Color.magenta, Color.cyan };
+        private int currentColorIndex = 0;
+
+        void Start()
+        {
+            meshRenderer = GetComponent<Renderer>();
+            if (meshRenderer == null)
+            {
+                Debug.LogError("No Renderer found on the GameObject.");
+            }
+        }
+
+        void OnMouseDown()
+        {
+            if (meshRenderer != null)
+            {
+                currentColorIndex = (currentColorIndex + 1) % colors.Length;
+                meshRenderer.material.color = colors[currentColorIndex];
+            }
+        }
+    }
+}
