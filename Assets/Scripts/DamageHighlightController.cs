6673ff4acf15470d973848fe40a4134c81bd30f0
Aimar Gonzalez <aimar.mel@gmail.com>
2025-03-12 03:03:38 +0100

    update DamageHighlightController.cs to animate the damage effect also in edit mode

--- Assets/Scripts/DamageHighlightController.cs
+++ Assets/Scripts/DamageHighlightController.cs
@@ -1,26 +1,54 @@
 using UnityEngine;
+using System;
+using UnityEngine.Serialization;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace ForestRoyale
 {
+    [ExecuteInEditMode]
     public class DamageHighlightController : MonoBehaviour
     {
-        private Material _material; 
         private float _currentIntensity = 0f;
         private float _targetIntensity = 0f;
-
-        [SerializeField]
-        private float _fadeSpeed = 2f;
         private bool _isFlashing = false;
-        private float _flashDuration = 0.5f;
         private float _flashTimer = 0f;
+
+        [SerializeField, Range(0, 1)]
+        private float _maxIntensity = 0.3f;
+       
+#if UNITY_EDITOR
+        // Use this param to previsualize the effect in EditMode
+        [SerializeField, Range(0, 1)]
+        private float _previewIntensity = 0f;
+#endif
+
+        [SerializeField, Min(0.001f)]
+        private float _flashDuration = 1f;
+        
+        [NonSerialized]
+        private Material _material; 
+
 
         void Start()
         {
             // Get the material from the renderer
+            InitializeMaterial();
+        }
+
+        private void InitializeMaterial()
+        {
+            if(_material != null)
+            {
+                return;
+            }
+
             Renderer renderer = GetComponent<Renderer>();
             if (renderer != null)
             {
-                _material = renderer.material;
+                _material = Application.isPlaying ? renderer.material : renderer.sharedMaterial;
             }
             else
             {
@@ -30,28 +58,83 @@ namespace ForestRoyale
 
         void Update()
         {
-            if (_material == null) return;
+            if (_material == null || !Application.isPlaying)
+                return;
 
+            UpdateDamageEffect(Time.deltaTime, ref _currentIntensity);
+        }
+
+        // This method contains the logic from the original Update method
+        private void UpdateDamageEffect(float deltaTime, ref float currentIntensity)
+        {
             if (_isFlashing)
             {
-                _flashTimer += Time.deltaTime;
+                _flashTimer += deltaTime;
                 if (_flashTimer >= _flashDuration)
                 {
                     _isFlashing = false;
                     _targetIntensity = 0f;
                 }
+           
+                // Smoothly interpolate the current intensity to the target
+                float progress = _flashTimer / _flashDuration;
+                currentIntensity = Mathf.Lerp(_maxIntensity, _targetIntensity, progress);
+              
+                UpdateMaterial(currentIntensity);
             }
+        }
 
-            // Smoothly interpolate the current intensity to the target
-            _currentIntensity = Mathf.Lerp(_currentIntensity, _targetIntensity, Time.deltaTime * _fadeSpeed);
-            _material.SetFloat("_DamageIntensity", _currentIntensity);
+         private void UpdateMaterial(float currentIntensity)
+        {
+            if (_material != null)
+            {
+                _material.SetFloat("_DamageIntensity", currentIntensity);
+            }
         }
 
         public void FlashDamage()
         {
             _isFlashing = true;
             _flashTimer = 0f;
-            _targetIntensity = 1f;
+            _targetIntensity = 0f;
+            _currentIntensity = _maxIntensity;
+                
+#if UNITY_EDITOR
+            _previewIntensity = _maxIntensity;
+#endif            
         }
+
+#if UNITY_EDITOR
+
+        void OnValidate()
+        { 
+             UpdateMaterial(_previewIntensity * _maxIntensity);
+        }
+
+        void OnEnable()
+        {
+            InitializeMaterial();
+            if (!Application.isPlaying)
+            {
+                EditorApplication.update += OnEditorUpdate;           
+            }
+        }
+
+        void OnDisable()
+        {
+            if (!Application.isPlaying)
+            {
+                EditorApplication.update -= OnEditorUpdate;
+            }
+        }
+
+        void OnEditorUpdate()
+        {
+            if (_material == null || Application.isPlaying)
+                return;
+            
+            UpdateDamageEffect(Time.deltaTime, ref _previewIntensity);
+        }
+#endif
     }
 } 
