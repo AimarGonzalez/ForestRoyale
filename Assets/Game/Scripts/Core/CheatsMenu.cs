b2fa727a6b067ae71c0d24dc08e78acd2beae38e
Aimar Gonzalez <aimar.mel@gmail.com>
2025-05-29 00:24:41 +0200

    adjust cheat panel to screen dimensions

--- Assets/Game/Scripts/Core/CheatsMenu.cs
+++ Assets/Game/Scripts/Core/CheatsMenu.cs
@@ -1,6 +1,9 @@
 using ForestRoyale.Core.UI;
 using ForestRoyale.Gameplay.Combat;
 using Sirenix.OdinInspector;
+using Sirenix.Utilities;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using VContainer;
 
@@ -15,22 +18,47 @@ namespace ForestRoyale.Core
 		[SerializeField]
 		private GUISkin _skin;
 
-		[BoxGroup(PANEL_SETTINGS), PropertyOrder(DebugUI.Order)]
-		[SerializeField] private int _fontSize = 12;
-		[BoxGroup(PANEL_SETTINGS), PropertyOrder(DebugUI.Order)]
-		[SerializeField] private float _labelWidth = 0.5f;
-		[BoxGroup(PANEL_SETTINGS), PropertyOrder(DebugUI.Order)]
-		[SerializeField] private int _slinderHeigh = 10;
+		[Serializable]
+		private struct PanelSettings
+		{
+			[Serializable]
+			private struct AspectRatio
+			{
+				public int Width;
+				public int Height;
 
-		[BoxGroup(PANEL_SETTINGS), PropertyOrder(DebugUI.Order)]
-		[SerializeField]
-		[Range(0.01f, 1f)]
-		private float _width = 0.5f;
+				public AspectRatio(int width, int height)
+				{
+					Width = width;
+					Height = height;
+				}
 
-		[BoxGroup(PANEL_SETTINGS), PropertyOrder(DebugUI.Order)]
+				public float Ratio => (float)Width / Height;
+			}
+
+			[SerializeField]
+			private AspectRatio _aspectRatio;
+
+			public float Ratio => _aspectRatio.Ratio;
+			public int FontSize;
+			public float LabelWidth;
+			public float Width;
+			public float Height;
+
+			public PanelSettings(int fontSize, float labelWidth, float width, float height)
+			{
+				_aspectRatio = new AspectRatio(1, 1);
+				FontSize = fontSize;
+				LabelWidth = labelWidth;
+				Width = width;
+				Height = height;
+			}
+		}
+
 		[SerializeField]
-		[Range(0.01f, 1f)]
-		private float _height = 0.3f;
+		[TableList]
+		[BoxGroup(PANEL_SETTINGS), PropertyOrder(DebugUI.Order)]
+		private List<PanelSettings> _panelSettings;
 
 		// ------------------
 
@@ -42,18 +70,61 @@ namespace ForestRoyale.Core
 
 		private void OnGUI()
 		{
-			GUIUtils.PushSkin(_skin);
-			GUIUtils.PushFontSize(_fontSize);
+			PanelSettings panelSettings = CalcInterpolatedPanelSettings();
 
-			GUILayoutUtils.LabelWidth = _labelWidth * _width * Screen.width;
+
+			GUIUtils.PushSkin(_skin);
+			GUIUtils.PushFontSize(panelSettings.FontSize);
+
+			GUILayoutUtils.LabelWidth = panelSettings.LabelWidth * panelSettings.Width * Screen.width;
 			GUILayoutUtils.LabelHeight = GUI.skin.label.CalcHeight(new GUIContent("X"), 100);
 
-			GUILayout.BeginArea(new Rect(10, 10, _width * Screen.width, _height * Screen.height), GUI.skin.box);
+			GUILayout.BeginArea(new Rect(10, 10, panelSettings.Width * Screen.width, panelSettings.Height * Screen.height), GUI.skin.box);
 			DrawCheatsGUI();
 			GUILayout.EndArea();
 
 			GUIUtils.PopFontSize();
 			GUIUtils.PopSkin();
+		}
+
+		private PanelSettings CalcInterpolatedPanelSettings()
+		{
+			if (_panelSettings.IsNullOrEmpty())
+			{
+				return new PanelSettings(12, 0.5f, 0.5f, 0.5f);
+			}
+
+			PanelSettings minSettings = _panelSettings[0];
+			PanelSettings maxSettings = _panelSettings[_panelSettings.Count - 1];
+			float screenRatio = (float)Screen.width / Screen.height;
+			foreach (PanelSettings panelSettings in _panelSettings)
+			{
+				if (screenRatio < panelSettings.Ratio && panelSettings.Ratio > minSettings.Ratio)
+				{
+					minSettings = panelSettings;
+				}
+
+				if (screenRatio > panelSettings.Ratio && panelSettings.Ratio < maxSettings.Ratio)
+				{
+					maxSettings = panelSettings;
+				}
+			}
+
+			float ratio = 1f;
+
+			if (minSettings.Ratio != maxSettings.Ratio)
+			{
+				ratio = (screenRatio - minSettings.Ratio) / (maxSettings.Ratio - minSettings.Ratio);
+			}
+
+			PanelSettings interpolatedSettings = new PanelSettings(
+				(int)Mathf.Lerp(minSettings.FontSize, maxSettings.FontSize, ratio),
+				Mathf.Lerp(minSettings.LabelWidth, maxSettings.LabelWidth, ratio),
+				Mathf.Lerp(minSettings.Width, maxSettings.Width, ratio),
+				Mathf.Lerp(minSettings.Height, maxSettings.Height, ratio)
+			);
+
+			return interpolatedSettings;
 		}
 
 		private void DrawCheatsGUI()
