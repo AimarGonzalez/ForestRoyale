847b5e9cfa8609ff5c1f618160c2d14e0237527a
Aimar Gonzalez <aimar.mel@gmail.com>
2025-05-30 14:07:02 +0200

    new Awaitable utility

---
+++ Assets/Libs/ForestLib/Utils/Awaitables.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace ForestLib.Utils
+{
+	public static class Awaitables
+	{
+		public static async Awaitable WaitUntil(Func<bool> condition)
+		{
+			while(!condition()){
+				await Awaitable.NextFrameAsync();
+			}
+		}
+	}
+}
